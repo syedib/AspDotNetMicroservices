5f3472b8199fe3914868150555e461681c38463f
syed <syedib@outlook.com>
2021-09-16 18:43:27 +0530

    Basket API changes completed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public IList<ShoppinCartItem> Items { get; set; } = new List<ShoppinCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = default;
+
+                foreach(var item in Items) 
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
